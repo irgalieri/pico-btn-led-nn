8e825bf9ab9c562d861da8f1f737284d32bad29f
Ignacio Rodrigo Galieri <ignacio.galieri@nubicall.com>
2022-07-20 16:39:48 -0500

    change to multi thread.

--- pico-btn-led-nn.c
+++ pico-btn-led-nn.c
@@ -1,41 +1,54 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
 
 #define PICO_MY_LED_PIN 15
 #define PICO_MY_BTN_PIN 14
+
+void blinking(void);
+void init_gpio_led(uint led);
+void init_gpio_btn(uint btn);
+void toggle_led(uint led);
 
 int main() {
 #ifndef PICO_DEFAULT_LED_PIN
 #warning blink example requires a board with a regular LED
 #else
     stdio_init_all();
-    const uint LED_PIN_EXT = PICO_MY_LED_PIN;
-    const uint LED_PIN_IN = PICO_DEFAULT_LED_PIN;
-    const uint BTN_PIN = PICO_MY_BTN_PIN;
 
-    gpio_init(LED_PIN_EXT);
-    gpio_set_dir(LED_PIN_EXT, GPIO_OUT);
+    init_gpio_led(PICO_DEFAULT_LED_PIN);
+    init_gpio_led(PICO_MY_LED_PIN);
+    init_gpio_btn(PICO_MY_BTN_PIN);
 
-    gpio_init(LED_PIN_IN);
-    gpio_set_dir(LED_PIN_IN, GPIO_OUT);
-
-    gpio_init(BTN_PIN);
-    gpio_set_dir(BTN_PIN, GPIO_IN);
-
-    printf("Starting Loop....");
+    multicore_launch_core1(blinking);
 
     while (1) {
-        if (gpio_get(BTN_PIN)) {
-            printf("Btn Press....");
-            gpio_put(LED_PIN_EXT, 0);
-        } else {
-            gpio_put(LED_PIN_EXT, 1);
+        if (gpio_get(PICO_MY_BTN_PIN)) {
+            toggle_led(PICO_MY_LED_PIN);
         }
-        gpio_put(LED_PIN_IN, 1);
-        sleep_ms(1000);
-        gpio_put(LED_PIN_IN, 0);
-        sleep_ms(250);
+        sleep_ms(100);
     }
 #endif
     return 0;
+}
+
+void init_gpio_led(uint led) {
+    gpio_init(led);
+    gpio_set_dir(led, GPIO_OUT);
+}
+
+void init_gpio_btn(uint btn) {
+    gpio_init(btn);
+    gpio_set_dir(btn, GPIO_IN);
+}
+
+void toggle_led(uint led) {
+    gpio_put(led, !gpio_get(led));
+}
+
+void blinking() {
+    while (1) {
+        toggle_led(PICO_DEFAULT_LED_PIN);
+        sleep_ms(1000);
+    }
 }
