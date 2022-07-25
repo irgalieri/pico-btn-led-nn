90c6ad68330765c1b6abf1fa1a7aa17132a63246
Nacho Galieri <irgalieri@gmail.com>
2022-07-25 05:25:56 -0500

    Add Pins Info to Binary

--- pico-btn-led-nn.c
+++ pico-btn-led-nn.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
 
 #define PICO_MY_LED_PIN 15
 #define PICO_MY_BTN_PIN 14
@@ -9,6 +11,9 @@ void blinking(void);
 void init_gpio_led(uint led);
 void init_gpio_btn(uint btn);
 void toggle_led(uint led);
+void init_onboard_led(void);
+void init_external_led(void);
+void init_external_btn(void);
 
 int main() {
 #ifndef PICO_DEFAULT_LED_PIN
@@ -16,9 +21,9 @@ int main() {
 #else
     stdio_init_all();
 
-    init_gpio_led(PICO_DEFAULT_LED_PIN);
-    init_gpio_led(PICO_MY_LED_PIN);
-    init_gpio_btn(PICO_MY_BTN_PIN);
+    init_onboard_led();
+    init_external_led();
+    init_external_btn();
 
     multicore_launch_core1(blinking);
 
@@ -30,6 +35,21 @@ int main() {
     }
 #endif
     return 0;
+}
+
+void init_onboard_led() {
+    bi_decl(bi_1pin_with_name(PICO_DEFAULT_LED_PIN, "On-Board LED"));
+    init_gpio_led(PICO_DEFAULT_LED_PIN);
+}
+
+void init_external_led() {
+    bi_decl(bi_1pin_with_name(PICO_MY_LED_PIN, "External LED"));
+    init_gpio_led(PICO_MY_LED_PIN);
+}
+
+void init_external_btn() {
+    bi_decl(bi_1pin_with_name(PICO_MY_BTN_PIN, "External Button"));
+    init_gpio_btn(PICO_MY_BTN_PIN);
 }
 
 void init_gpio_led(uint led) {
